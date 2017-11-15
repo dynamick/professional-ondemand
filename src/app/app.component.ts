ec1ef9a3390931a67bb00007c5622a0e3f0c078c
Michele Gobbi <info@dynamick.it>
2017-11-15 19:13:44 +0100

    bozza users list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:  `
+    <h1>Layout principale</h1>
+
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/views/users/users.module.ts
+++ src/app/views/users/users.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {UserListComponent} from './components/user-list.component';
+import {users_routing} from './users.routing';
 
 @NgModule({
   declarations: [
     UserListComponent
   ],
   imports: [
+    users_routing
   ],
   providers: [
   ],
